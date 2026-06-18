a8b2a89fb2bbb6df94bc2c7a07a53b18c2f1f2be
pythongabrieljq-afk <python.gabriel.jq@gmail.com>
2026-06-18 17:31:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import io
+from typing import Optional
+
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
+from pymongo import MongoClient
 
 st.set_page_config(
     page_title="Vigitel Dashboard",
